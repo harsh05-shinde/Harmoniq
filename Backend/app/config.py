66b390e1d3cb9e073e634bb462ac3d2d4d8357d6
harsh05-shinde <shindeharshwardhan1000@gmail.com>
2026-09-09 16:43:13 +0530

    Configure Aiven MySQL for deployment

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -6,4 +6,20 @@ DATABASE_URL =  os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_TIME = int(os.getenv("ACCESS_TOKEN_EXPIRE_TIME"))
+SSL_CA =  os.getenv("SSL_CA")
+
+
+if SSL_CA:
+    if not os.path.isabs(SSL_CA):
+        SSL_CA = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            SSL_CA
+        )
+
+
+
+
+
+
+
 
--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from app.config import DATABASE_URL
+from app.config import DATABASE_URL , SSL_CA
 
-engine = create_engine(DATABASE_URL)
+
+connect_args  = {}
+
+if SSL_CA:
+    connect_args = {
+        "ssl_ca":SSL_CA,
+        "ssl_verify_cert":True,
+        "ssl_verify_identity":True
+    }
+
+engine = create_engine(DATABASE_URL ,
+                       connect_args=connect_args,
+                       pool_pre_ping=True)
 
 Sessionlocal = sessionmaker(bind=engine)
 
